defc0949ac42229e7741bce8f66c45bd9f2685b8
Xx-Geovane-xX <geovane08almeida@gmail.com>
2024-10-16 13:49:03 -0300

    studies c#

---
+++ teste && aulas/teste1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class aa
+{
+
+    static void teste
+    {
+        int a = 2;
+        int b = 3;
+
+        int soma = a + b;
+
+        console.WriteLine("opa: " + soma);
+}
+}
+
+Console.WriteLine("Hello, World!");
---
+++ teste && aulas/teste2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+class Teste
+{
+    static void Main()
+    {
+
+        Func<int, int, int> teste = delegate (int xx, int yy) { return xx + yy; };
+        Func<string, string, bool> teste1 = (e, b) => e.Equals(b, StringComparison.OrdinalIgnoreCase);;
+        Console.WriteLine("teste " + teste);
+        Console.WriteLine(teste1("be", "BE"));
+    }
+}
